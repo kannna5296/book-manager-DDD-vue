68d756e6ca6b7fd957425cbd14dad631192e5b9c
kannna5296 <kannnahacks5296@gmail.com>
2022-05-14 18:02:07 +0900

    Welcomeページ生成

--- book-manager/src/router/index.js
+++ book-manager/src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import WelcomeView from '../views/WelcomeView'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'WelcomeView',
+    component: WelcomeView
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
